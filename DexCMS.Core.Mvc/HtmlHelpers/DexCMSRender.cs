1f39d125c47516b56b5df55366200b2880e415f1
Cyricx <contact@chrisbyram.com>
2016-11-20 11:55:37 -0600

    Added html render to add version number from web config

---
+++ DexCMS.Core.Mvc/HtmlHelpers/DexCMSRender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Optimization;
+
+namespace DexCMS.Core.Mvc.HtmlHelpers
+{
+    public static class DexCMSRender
+    {
+        private static string Version = WebConfigurationManager.AppSettings["Version"];
+        private static string VersionQueryString = WebConfigurationManager.AppSettings["VersionQueryString"];
+
+        public static IHtmlString StylesWithVersion(params string[] paths)
+        {
+            return Styles.Render(AddVersionToPaths(paths));
+        }
+
+        public static IHtmlString ScriptsWithVersion(params string[] paths)
+        {
+            return Scripts.Render(AddVersionToPaths(paths));
+        }
+
+        private static string[] AddVersionToPaths(params string[] paths)
+        {
+            return paths.Select(x => BuildWithVersion(x)).ToArray();
+        }
+
+        private static string BuildWithVersion(string path)
+        {
+            return path + (path.Contains("?") ? "&" : "?") + VersionQueryString + "=" + Version;
+        }
+    }
+}
