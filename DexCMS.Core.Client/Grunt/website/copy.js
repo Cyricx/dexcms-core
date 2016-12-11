f6b65c558f13cc2845d2addb634984d3b1094682
Cyricx <contact@chrisbyram.com>
2016-12-11 05:50:02 -0600

    Refactored grunt copy and added default content to mvc.

--- DexCMS.Core.Client/Grunt/website/copy.js
+++ DexCMS.Core.Client/Grunt/website/copy.js
@@ -1,114 +1,86 @@
-﻿var _modulesBuilder = function (options) {
-    var copyModules = [];
-
-    var _buildCopyModule = function (module) {
-        return {
-            expand: true,
-            cwd: options.nodelibs + '/dexcms-' + module + '/dexcms.' + module + '.client/scripts/',
-            src: ['**'],
-            dest: options.applicationsPath
-        };
+﻿var _constructCopy = function (directory, source, destination, shouldExpand) {
+    shouldExpand = shouldExpand || true;
+    return {
+        expand: shouldExpand,
+        cwd: directory,
+        src: source,
+        dest: destination
     };
+}
 
-    for (var i = 0; i < options.modules.length; i++) {
-        copyModules.push(_buildCopyModule(options.modules[i]));
-    }
+var _buildClient = function (options) {
+    var copyModules = [];
+    var directory;
+
+    options.modules.forEach(function (mod) {
+        directory = options.nodelibs + '/dexcms-' + module + '/dexcms.' + module + '.client/scripts/';
+        copyModules.push(_constructCopy(directory, ['**'], options.applicationsPath));
+    });
 
     return copyModules;
 };
 
-var _vendorFileBuilder = function (options) {
+var _buildVendor = function (options) {
     var copyFiles = [];
+    var directory, destination;
 
-    //dynamic vendorFiles
     if (options.vendorFiles.content) {
-        for (var i = 0; i < options.vendorFiles.content.length; i++) {
-            copyFiles.push({
-                expand: true,
-                cwd: options.bower + '/' + options.vendorFiles.content[i].folder + '/',
-                src: options.vendorFiles.content[i].files,
-                dest: options.libs + '/' + (options.vendorFiles.content[i].dist || options.vendorFiles.content[i].folder) + '/'
-            });
-        }
+        options.vendorFiles.content.forEach(function (vendorFile) {
+            directory = options.bower + '/' + vendorFile.folder + '/';
+            destination = options.libs + '/' + (vendorFile.dist || vendorFile.folder) + '/';
+
+            copyFiles.push(_constructCopy(directory, vendorFile.files, destination));
+        });
     };
 
     if (options.vendorFiles.fonts) {
-        for (var i = 0; i < options.vendorFiles.fonts.length; i++) {
-            var dest = (options.vendorFiles.fonts[i].dist == '') ? '' : (options.vendorFiles[i].dist || options.vendorFiles[i].folder) + '/';
-
-            copyFiles.push({
-                expand: true,
-                cwd: options.bower + '/' + options.vendorFiles.fonts[i].folder + '/',
-                src: options.vendorFiles.fonts[i].files,
-                dest: options.fonts + '/' + dest
-            });
-        }
+        options.vendorFiles.fonts.forEach(function (vendorFont) {
+            var vendorFolder = (vendorFont.dist == '') ? '' : (options.vendorFiles[i].dist || options.vendorFiles[i].folder) + '/';
+            destination = options.fonts + '/' + vendorFolder;
+            directory = options.bower + '/' + vendorFont.folder + '/';
+            
+            copyFiles.push(_constructCopy(directory, vendorFont.files, destination));
+        });
     };
 
     return copyFiles;
 };
 
-var _viewsBuilder = function (options) {
-    var copyDefaultViews = [];
+var _buildMvc = function (options) {
+    var copyFiles = [];
+    var baseDirectory, modulePath;
 
-    var _buildViews = function (mod) {
-        return [
-            {
-                expand: true,
-                cwd: options.nodelibs + '/' + mod + '/defaultviews/rootviews/',
-                src: ['**'],
-                dest: 'views/dexcms/'
-            },
-            {
-                expand: true,
-                cwd: options.nodelibs + '/' + mod + '/defaultviews/areaviews/',
-                src: ['**'],
-                dest: 'views/dexcms-areas/'
-            },
-            {
-                expand: true,
-                cwd: options.nodelibs + '/' + mod + '/defaultscripts/',
-                src: ['**'],
-                dest: 'scripts/DexCMS/'
-            }
-        ];
-    };
+    options.modules.forEach(function (module) {
+        modulePath = options.nodelibs + '/dexcms-' + module.toLowerCase() + '/dexcms.' + module.toLowerCase() + '.mvc';
+        
+        copyFiles.push(_constructCopy(modulePath + '/defaultviews/rootviews/', ['**'], 'views/dexcms/'));
+        copyFiles.push(_constructCopy(modulePath + '/defaultviews/areaviews/', ['**'], 'views/dexcms-areas/'));
+        copyFiles.push(_constructCopy(modulePath + '/defaultscripts/', ['**'], 'scripts/DexCMS/'));
+        copyFiles.push(_constructCopy(modulePath + '/defaultcontent/', ['**'], 'content/'));
+    });
 
-
-
-    for (var i = 0; i < options.modules.length; i++) {
-        var mod = 'dexcms-' + options.modules[i].toLowerCase() + '/dexcms.' + options.modules[i].toLowerCase() + '.mvc';
-        copyDefaultViews.push.apply(copyDefaultViews, _buildViews(mod));
-    }
-
-    return copyDefaultViews;
+    return copyFiles;
 };
 
 var copyGrunt = function (grunt, options) {
-
+    
     var copy = {
         vendorFiles: {
-            files: _vendorFileBuilder(options)
+            files: _buildVendor(options)
         },
         defaultViews: {
-            files: _viewsBuilder(options)
+            files: _buildMvc(options)
         },
         applicationsModules: {
-            files: _modulesBuilder(options)
+            files: _buildClient(options)
         }
     };
 
     if (options.customModulesPath) {
-        copy.dexCMSApplicationsCustom = {
-            expand: true,
-            cwd: options.customModulesPath,
-            src: ['**'],
-            dest: options.applicationsPath
-        }
+        copy.dexCMSApplicationsCustom = _constructCopy(options.customModulesPath, ['**'], options.applicationsPath);
     }
 
     return copy;
-
 };
 
 module.exports = copyGrunt;
---
+++ DexCMS.Core.Infrastructure/Initializers/ImageInitializer.cs
@@ -0,0 +1,24 @@
+﻿using DexCMS.Core.Infrastructure.Contexts;
+using DexCMS.Core.Infrastructure.Extensions;
+using DexCMS.Core.Infrastructure.Globals;
+using DexCMS.Core.Infrastructure.Models;
+using System.Data.Entity.Migrations;
+
+namespace DexCMS.Core.Infrastructure.Initializers
+{
+    class ImageInitializer : DexCMSInitializer<IDexCMSCoreContext>
+    {
+        public ImageInitializer(IDexCMSCoreContext context) : base(context)
+        {
+        }
+
+        public override void Run()
+        {
+            Context.Images.AddIfNotExists(x => x.Alt,
+                new Image { Alt = "Gaea Retreat", Caption = "Gaea Retreat Center", Credit = "Chris Byram", Original = "GaeaRetreat_original.png", Gallery = "GaeaRetreat_gallery.jpg", Slider = "GaeaRetreat_slider.jpg", Thumbnail = "GaeaRetreat_thumbnail.jpg" },
+                new Image { Alt = "Lawrence Busker", Caption = "Lawrence Busker Festival", Credit = "Chris Byram", Original = "LawrenceBusker_original.png", Gallery = "LawrenceBusker_gallery.jpg", Slider = "LawrenceBusker_slider.jpg", Thumbnail = "LawrenceBusker_thumbnail.jpg" }
+            );
+            Context.SaveChanges();
+        }
+    }
+}
