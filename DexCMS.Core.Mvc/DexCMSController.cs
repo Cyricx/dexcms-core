882ab4f8475774e86ac3c9f538dc58b4616a61b4
Cyricx <contact@chrisbyram.com>
2016-12-23 22:58:01 -0600

    MVC: Working on global error handling

---
+++ DexCMS.Core.Mvc/DexCMSController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DexCMS.Core.Mvc
+{
+    public class DexCMSController: Controller
+    {
+            protected override void OnException(ExceptionContext filterContext)
+            {
+                Exception ex = filterContext.Exception;
+                filterContext.ExceptionHandled = true;
+            
+                var model = new HandleErrorInfo(filterContext.Exception, filterContext.RouteData.Values["Controller"].ToString(), filterContext.RouteData.Values["Action"].ToString());
+
+                filterContext.Result = new ViewResult()
+                {
+                    ViewName = "Error",
+                    ViewData = new ViewDataDictionary(model)
+                };
+
+            }
+    }
+}
